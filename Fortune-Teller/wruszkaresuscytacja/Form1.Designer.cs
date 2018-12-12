45ff35d3aa523d9611d69a15fffc2821c6b36af5
Kikker17 <43655001+Kikker17@users.noreply.github.com>
2018-12-12 23:06:13 +0100

    init-for-real

---
+++ Fortune-Teller/wruszkaresuscytacja/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace wruszkaresuscytacja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Arial Black", 14.8F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(248, 560);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Prophecy";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Arial Black", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(41, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(610, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Do you want to know your future? If so, click \"prophecy\"...";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(698, 701);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Fortune-Teller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Fortune-Teller/wruszkaresuscytacja/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Globalization;
+
+namespace wruszkaresuscytacja
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random los = new Random();
+/*            string[] I = { "Jutro, ", "Za pięć minut, ", "Za dwa lata, ", "Na następnym egzaminie, ", "W twoje najbliższe urodziny, ", "Na starość, ", "Na najbliższej imprezie, ", "Za tydzień, " };
+            string[] II = { "zjesz żabę, ", "nic ci się nie uda, ", "poznasz swoją drugą połówkę, ", "zaczniesz tańczyć kankana na stole w obecności znajomych, ", "nie dstaniesz listu z Hogwartu, ", "upijesz się bezalkoholowym szampanem dla dzieci, ", "zostaniesz gwiazdą disko, ", "spotkasz Gandalfa, ", "porwą cię kosmici, ", "poznasz prawdziwego jednorożca, " };
+            string[] III = { "i ", "potem ", "następnie ", "chwilę później ", "oraz ", "a także ", "a następnego dnia " };
+            string[] IV = { "podejmiesz decyzję, która zmieni całe twoje życie, ", "zawalisz egzamin, ", "zmienisz płeć, ", "zjesz pizzę, ", "zrobisz sobie operację plastyczną, ", "zrobisz sobie tatuaż na kolanie, ", "zrobisz sobie tatuaż na brzuchu, ", "zrobisz sobie tatuaż na pięcie, ", "zrobisz sobie tatuaż na łokciu, ", "kupisz bezglutenowy ser bez laktozy, " };
+            string[] V = { "więc ", "dlatego ", "co sprawi, że " };
+            string[] VI = { "zaznasz szczęścia.", "rozpłaczesz się.", "zostaniesz klaunem.", "odkryjesz sens swojego życia.", "dostaniesz pracę w ministerstwie edukacji.", "zwątpisz w sens życia.", "rozpoczniesz pracę w Makdonaldzie.", "weźmiesz ślub dwa dni później.", "rozpoczniesz karierę babci klozetowej.", "obejrzysz nowy odcinek kucy z Bronksu.", "zamienisz się w wilkołaka." };
+*/
+            string [] I = {"Tomorrow, ", "In five minutes, ", "In two years, ", "At the next exam, ", "At your next birthday, ", "When you're old, ", "When you're sixty-four, ", "At the next party, ", "Next week, " };
+            string [] II = {"you will eat a frog, ", "you will fail, ", "you will fall in love, ", "you will start to dance cancan on the table in the presence of your friends, ", "you won't get a letter from Hogwarts, ", "you will drink a non-alcoholic champagne for children, ", "you will become a disco star, ", "aliens will abduct you, ", "you will meet a real unicorn, "};
+            string [] III = {"and ", "then ", "a moment later ", "also ", "the next day "};
+            string [] IV = {"you will make a decision, that will change your whole life, ", "you won't pass your exam, ", "you will change gender, ", "you will eat pizza, ", "you will undergo plastic surgery, ", "you will get a tattoo on your knee , ", " you will get a tattoo on your belly, ", " you will get a tattoo on your heel, ", " you will get a tattoo on the elbow, ", "you will buy gluten-free cheese without lactose, "};
+            string [] V = {"so ", "and because of that, "};
+            string [] VI = {"you will feel happy.", "you will cry.", "you will become a clown.", "you will discover the meaning of your life.", "you will get a job in the ministry of education.", "you will doubt in the meaning of life.", " you will start working in McDonald's."," you will get married two days later."," you will start your toilet grandmother's career."," you will see a new episode of the Ponies from Bronx."," you will turn into a werewolf."};
+
+            // Generate random indexes for pet names.
+            int iIndex = los.Next(0, I.Length);
+            int iiIndex = los.Next(0, II.Length);
+            int iiiIndex = los.Next(0, III.Length);
+            int ivIndex = los.Next(0, IV.Length);
+            int vIndex = los.Next(0, V.Length);
+            int viIndex = los.Next(0, VI.Length);
+
+            string tekst = string.Concat(I[iIndex], II[iiIndex], III[iiiIndex], IV[ivIndex], V[vIndex], VI[viIndex]);
+
+            Console.WriteLine(tekst);
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Volume = 100;
+            synth.Rate = -6;
+            synth.SetOutputToDefaultAudioDevice();
+            synth.Speak(tekst);
+        }
+    }
+}
